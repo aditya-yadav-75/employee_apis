44c7ea62705fbfedf513eafde45994ec8a6029b1
Aditya Yadav <adityayadav26official@gmail.com>
2026-08-14 10:42:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const db = require("./config/db");
+const db=require("./config/db");
 const employeeRouter=require("./router/employeeRouter");
-
+const cors=require('cors');
 const app=express();
 app.use(express.json());
+app.use(cors('*'));
 app.use("/employees",employeeRouter);
 
 app.listen(4000,()=>{
